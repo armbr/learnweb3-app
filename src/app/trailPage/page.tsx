f04ce235d238aeb663819a0bb423496d90ee5b5a
aaasoares01 <kawaibonilhasoares@gmail.com>
2024-10-03 18:04:33 -0300

    Making some components and pages

--- src/app/trailPage/page.tsx
+++ src/app/trailPage/page.tsx
@@ -1,5 +1,11 @@
-import { TrailCards } from "@/components/TrailPage/TrailContainer";
+import { Above } from "@/components/TrailPage/Above";
+import { Container } from "@/components/TrailPage/Container";
 
 export default function trailPage() {
-  return <TrailCards />;
+  return (
+    <div className="gap-3 flex flex-col justify-center items-center">
+      <Container />
+      <Above />
+    </div>
+  );
 }
---
+++ src/app/trailsPage/page.tsx
@@ -0,0 +1,5 @@
+import { TrailCards } from "@/components/TrailsPage/TrailContainer";
+
+export default function trailsPage() {
+  return <TrailCards />;
+}
