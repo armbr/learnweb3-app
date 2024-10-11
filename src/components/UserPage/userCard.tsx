614d75d0336f3201d5872d7e9493a7dff0001e34
EmeteZ <125473215+EmeteZ@users.noreply.github.com>
2024-10-11 18:18:12 -0300

    update UserPage

--- src/components/UserPage/userCard.tsx
+++ src/components/UserPage/userCard.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import UserImage from "../../assets/images/UserImage.png";
 import Image from "next/image";
-import { IMaskInput } from "react-imask";
+import { MdEdit } from "react-icons/md";
+import { FaSave } from "react-icons/fa";
+import { MotionButton } from "../ui/Button";
 
 export const UserSection = () => {
+  function teste() {
+    console.log("teste");
+  }
   return (
-    <div className="w-2/4 h-[33rem] bg-cgray shadow-xl  border-2 border-gray rounded-box">
+    <div className="w-2/4 h-[33rem] bg-cgray shadow-xl flex flex-col border-2 border-gray rounded-[2rem] p-5">
       <div className="flex flex-row m-4 gap-8 justify-center">
         <div role="tablist" className="tabs tabs-bordered gap-8">
           <a role="tab" className="tab hover:tab-active text-lg text-dgray">
@@ -21,11 +28,11 @@ export const UserSection = () => {
           </a>
         </div>
       </div>
-      <div className="flex flex-col ml-8 mt-12">
-        <div className="flex mb-8 ">
+      <div className="flex flex-col flex flex-col gap-6 ">
+        <div className="flex">
           <p className="text-dgray text-2xl font-medium">Dados Pessoais</p>
         </div>
-        <div className="flex-row flex">
+        <div className="flex-row flex justify-between">
           <div className="flex flex-row justify-center items-center">
             <Image src={UserImage} alt="" className=" w-20 h-20 rounded-full" />
             <div className="flex flex-col items-left ml-4">
@@ -34,12 +41,16 @@ export const UserSection = () => {
               </p>
               <p className="text-dgray ">@mtburgerking</p>
             </div>
-            <button className="btn btn-sm ml-14 text-dblue bg-cyellow hover:bg-dyellow">
-              Editar Imagem
-            </button>
           </div>
+          <MotionButton
+            Icon={MdEdit}
+            label="Editar Imagem"
+            func={() => teste}
+            className="bg-green text-ddblue h-9 items-center w-40"
+            type="button"
+          />
         </div>
-        <label className="flex flex-row h-full w-full gap-8 mt-6 justify-center">
+        <label className="flex flex-row h-full w-full gap-8 justify-center">
           <div className="flex flex-col w-full">
             <div className="label">
               <span className="label-text text-dgray">Nome</span>
@@ -77,6 +88,15 @@ export const UserSection = () => {
             />
           </div>
         </label>
+        <div className="flex justify-end w-full">
+          <MotionButton
+            Icon={FaSave}
+            label="Salvar"
+            func={() => teste}
+            className="bg-green text-ddblue"
+            type="button"
+          />
+        </div>
       </div>
     </div>
   );
