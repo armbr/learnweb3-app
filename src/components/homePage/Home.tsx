cb01c9c338ff5a066ccc567185ca9849836e1d98
Thiago Ulloa <84195990+thiagoulloa@users.noreply.github.com>
2024-10-30 15:48:42 -0300

    navbar + sections update

--- src/components/homePage/Home.tsx
+++ src/components/homePage/Home.tsx
@@ -6,7 +6,7 @@ import { TrailsCardSection } from "./TrailsCardSection";
 
 export const Home = () => {
   return (
-    <div className="h-full w-full grid items-center grid-cols-1 lg:grid-rows-5 pb-6 lg:grid-cols-5 lg:px-40 px-10 justify-center gap-10 overflow-y-auto">
+    <div className="h-full w-full grid items-center grid-cols-1 lg:grid-rows-5 pb-6 lg:grid-cols-5 lg:px-40 px-10 justify-center gap-10">
       <UserCard />
       <NftsCard />
       <JourneysCard />
