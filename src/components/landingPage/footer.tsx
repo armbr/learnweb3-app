f3456e4f832ccec4887ffc03ee87d02e33e63280
Thiago Ulloa <84195990+thiagoulloa@users.noreply.github.com>
2024-10-30 22:28:00 -0300

    landing page mobile update

--- src/components/landingPage/footer.tsx
+++ src/components/landingPage/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-neutralbg text-neutral text-center py-4 w-full">
+    <footer className="bg-neutralbg text-neutral text-center w-full py-2 px-4 md:py-4">
       <p>© 2024 Web3EduBrasil. Todos os direitos reservados.</p>
     </footer>
   );
