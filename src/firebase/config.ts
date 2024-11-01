547d849de65d9bb46f74065a4471b1e6ada71290
Thiago Ulloa <84195990+thiagoulloa@users.noreply.github.com>
2024-11-01 12:39:08 -0300

    update web3auth

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,51 +1,23 @@
-import firebase from "firebase/compat/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+const db = getFirestore(app);
 
-const db = firebase.firestore();
-
-// Inserindo um documento com um ID específico
-db.collection('Users').doc('user').set({
-  name: 'Kiwi',
-  age: 10,
-  email: 'kiwi@example.com'
-})
-.then(() => {
-  console.log('Documento criado com sucesso!');
-})
-.catch((error) => {
-  console.error('Erro ao criar documento: ', error);
-});
-
-db.collection('Users').doc('user').set({
-  name: 'thigas',
-  age: 40,
-  email: 'thigas@example.com'
-})
-.then(() => {
-  console.log('Documento criado com sucesso!');
-})
-.catch((error) => {
-  console.error('Erro ao criar documento: ', error);
-});
-
-db.collection('Users').doc('user').set({
-  name: 'Big',
-  age: 20,
-  email: 'big@example.com'
-})
-.then(() => {
-  console.log('Documento criado com sucesso!');
-})
-.catch((error) => {
-  console.error('Erro ao criar documento: ', error);
-});
+export { app, auth, storage, db };
