0034bf76efcdf7850f509432296807b4d7e5be32
EmeteZ <mtmacedo17@gmail.com>
2024-12-02 22:45:43 -0300

    UserEdit Fetch

--- src/components/UserPage/userCard.tsx
+++ src/components/UserPage/userCard.tsx
@@ -1,15 +1,67 @@
 "use client";
 
-import UserImage from "../../assets/images/UserImage.png";
 import Image from "next/image";
 import { MdEdit } from "react-icons/md";
 import { FaSave } from "react-icons/fa";
 import { MotionButton } from "../ui/Button";
+import useWeb3Auth from "@/lib/web3auth/web3auth";
+import { useWeb3AuthContext } from "@/lib/web3auth/Web3AuthProvider";
+import { useEffect, useState } from "react";
+import { getDisplayName } from "next/dist/shared/lib/utils";
+import { Bounce, toast } from "react-toastify";
 
 export const UserSection = () => {
   function teste() {
     console.log("teste");
   }
+
+  const { userDbInfo, googleUserInfo, fetchUserDbData } = useWeb3AuthContext();
+
+  const [userName, setUserName] = useState("");
+  const [linkedin, setLinkedin] = useState("");
+  const [discord, setDiscord] = useState("");
+
+  useEffect(() => {
+    console.log(userDbInfo, googleUserInfo);
+    setUserName(userDbInfo?.displayName);
+    setLinkedin(userDbInfo?.linkedin);
+    setDiscord(userDbInfo?.discord);
+  }, [userDbInfo]);
+
+  const fetchUserEdit = async () => {
+    try {
+      const response = await fetch("/api/user/edit", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          uid: googleUserInfo?.uid,
+          displayName: userName,
+          socialMedia: {
+            linkedin: linkedin,
+            discord: discord,
+          },
+        }),
+      });
+      if (response.ok) {
+        fetchUserDbData(googleUserInfo?.uid);
+        console.log(response.json());
+        toast.success("Dados Atualizados com Sucesso!", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+        });
+      }
+    } catch (error: any) {
+      console.error("Erro ao editar os dados do usúario", error);
+    }
+  };
+
   return (
     <div className="flex w-full h-full justify-center items-center">
       <div className=" w-4/5 h-[26rem] md:w-2/5 md:h-[30rem] bg-cgray shadow-xl flex flex-col border-2 border-gray rounded-[2rem] pt-2 md:pt-7 p-7">
@@ -52,17 +104,14 @@ export const UserSection = () => {
           </div>
           <div className="md:flex-row flex-col flex gap-4 justify-between">
             <div className="flex flex-row justify-center items-center">
-              <Image
-                src={UserImage}
+              <img
+                src={googleUserInfo?.photoURL}
                 alt=""
-                className=" w-10 h-10 md:w-16 md:h-16  rounded-full"
-              />
+                className=" w-10 h-10 md:w-16 md:h-16 rounded-full"
+              ></img>
               <div className="flex flex-col items-left ml-4">
                 <p className="text-dgray font-medium text-sm md:text-lg">
-                  Martin Burger King
-                </p>
-                <p className="text-dgray text-sm md:text-base ">
-                  @mtburgerking
+                  {userDbInfo.displayName}
                 </p>
               </div>
             </div>
@@ -83,6 +132,8 @@ export const UserSection = () => {
                 <span className="md:text-sm text-xs  text-dgray">Nome</span>
               </div>
               <input
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
                 type="text"
                 placeholder=""
                 className="input input-bordered w-full md:h-10 h-8 bg-white md:text-base text-xs md:rounded-box border-2 border-gray text-dgray"
@@ -91,26 +142,30 @@ export const UserSection = () => {
                 <span className="md:text-sm text-xs text-dgray">Email</span>
               </div>
               <input
+                value={userDbInfo.email}
                 type="text"
+                disabled={true}
                 placeholder=""
                 className="input input-bordered w-full md:h-10 h-8 bg-white md:text-base text-xs md:rounded-box border-2 border-gray text-dgray"
               />
             </div>
             <div className="flex flex-col w-full">
               <div className="label">
-                <span className="md:text-sm text-xs text-dgray">
-                  Nome de Usuario
-                </span>
+                <span className="md:text-sm text-xs text-dgray">Discord</span>
               </div>
               <input
+                value={discord}
+                onChange={(e) => setDiscord(e.target.value)}
                 type="text"
                 placeholder=""
                 className="input input-bordered w-full md:h-10 h-8  bg-white md:text-base text-xs md:rounded-box border-2 border-gray text-dgray"
               />
               <div className="label">
-                <span className="md:text-sm text-xs text-dgray">Telefone</span>
+                <span className="md:text-sm text-xs text-dgray">Linkedin</span>
               </div>
               <input
+                value={linkedin}
+                onChange={(e) => setLinkedin(e.target.value)}
                 type="text"
                 placeholder=""
                 className="input input-bordered w-full md:h-10 h-8 bg-white md:text-base text-xs md:rounded-box border-2 border-gray text-dgray"
@@ -121,7 +176,7 @@ export const UserSection = () => {
             <MotionButton
               Icon={FaSave}
               label="Salvar"
-              func={() => teste}
+              func={() => fetchUserEdit()}
               className=" flex justify-center text-xs items-center  h-7 w-[6rem] bg-green text-ddblue md:text-sm md:h-8 md:w-36"
               type="button"
             />
