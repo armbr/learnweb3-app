25940e263e9cc8ebbc7ca576d7864e4095e7dbdc
pedroco3lho <pedrocoelho.nascimento@gmail.com>
2025-04-04 14:57:36 -0300

    fix: AI Api structure

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -10,17 +10,17 @@ function systemMessage(question: string) {
 Caso a declaração seja válida, defina 'valido: true' e forneça uma breve explicação afirmando que está correta. Caso seja inválida, defina 'valido: false' e explique de forma clara e objetiva o que está incorreto.`;
 }
 
-export const POST = async (param: NextRequest) => {
+export const POST = async (req: NextRequest) => {
   try {
-    const { question, prompt } = await param.json();
+    const { question, prompt } = await req.json();
 
     if (prompt.length > 500) return NextResponse.json({error: "Prompt too long, must be under 500 characters" }, { status: 400 });
 
-    const req = await model.generateContent({
+    const modelResponse = await model.generateContent({
       systemInstruction: systemMessage(question),
       contents: prompt,
     });
-    const response = req.response;
+    const response = modelResponse.response;
     const result = await response.text();
     return NextResponse.json({ body: result }, { status: 201 });
   } catch (error: any) {
