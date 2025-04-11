a04e087846d07e8ea117a8b22e3427637bbb5725
pedroco3lho <pedrocoelho.nascimento@gmail.com>
2025-04-11 17:38:24 -0300

    feat: enable app metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+// app/layout.tsx (sem "use client"!)
 import "../styles/globals.css";
 import { Web3AuthProvider } from "@/lib/web3auth/Web3AuthProvider";
 import NavBar from "@/components/NavBar/NavBar";
@@ -8,31 +7,34 @@ import { ToastContainer } from "react-toastify";
 import { RewardContainer } from "@/components/RewardContainer/RewardContainer";
 import { Lexend_Deca } from "next/font/google";
 import { LoadingProvider } from "@/lib/loading-context";
+import type { Metadata } from "next";
 
-/* export const metadata: Metadata = {
+const LexendDeca = Lexend_Deca({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export const metadata: Metadata = {
   title: "Web3EduBrasil",
   description:
-    "Web3EduBrasil é uma plataforma de ensino descentralizada que facilita o aprendizados de tecnologias web3.",
+    "Web3EduBrasil é uma plataforma de ensino descentralizada que facilita o aprendizado de tecnologias web3.",
   openGraph: {
     title: "Web3EduBrasil - Aprenda sobre Web3 e conquiste NFTs únicas.",
     description:
-      "Web3EduBrasil é uma plataforma de ensino descentralizada que facilita o aprendizados de tecnologias web3.",
+      "Web3EduBrasil é uma plataforma de ensino descentralizada que facilita o aprendizado de tecnologias web3.",
     url: process.env.NEXT_PUBLIC_APP_LINK,
     siteName: "Web3EduBrasil",
-    // images: [
-    //     {
-    //         url: "/visuals/starknetquest.webp",
-    //         width: 680,
-    //         height: 680,
-    //     },
-    // ],
     locale: "pt_BR",
-    alternateLocale: "en_US",
-    countryName: "Brazil",
     type: "website",
+    images: [
+      {
+        url: "/visuals/landing.jpg",
+        width: 680,
+        height: 680,
+      },
+    ],
   },
   twitter: {
-    siteId: "@web3edubrasil",
     site: "@web3edubrasil",
     card: "summary_large_image",
   },
@@ -48,14 +50,11 @@ import { LoadingProvider } from "@/lib/loading-context";
     "web3educacao",
     "web3edu",
     "web3edubrasil",
-    "web3 brasil"
+    "web3 brasil",
+    "blockchain brasil",
+    "brasil",
   ],
-}; */
-
-const LexendDeca = Lexend_Deca({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-});
+};
 
 export default function RootLayout({
   children,
@@ -63,13 +62,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <LoadingProvider>
-      <Web3AuthProvider>
-        <ContentProvider>
-          <html lang="pt-br">
-            <head />
-
-            <body className={LexendDeca.className}>
+    <html lang="pt-br">
+      <body className={LexendDeca.className}>
+        <LoadingProvider>
+          <Web3AuthProvider>
+            <ContentProvider>
               <main
                 className="flex w-full flex-col items-center bg-neutralbg justify-start h-screen overflow-hidden "
                 data-theme="light"
@@ -92,10 +89,10 @@ export default function RootLayout({
                   theme="light"
                 />
               </main>
-            </body>
-          </html>
-        </ContentProvider>
-      </Web3AuthProvider>
-    </LoadingProvider>
+            </ContentProvider>
+          </Web3AuthProvider>
+        </LoadingProvider>
+      </body>
+    </html>
   );
 }
