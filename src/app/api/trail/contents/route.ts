c701a1e1b53b8942c75dea49e16dca66755f63ea
armbr <armbr258@gmail.com>
2025-10-10 15:39:09 -0300

    Update route.ts
    added export const dynamic = 'force-dynamic';

--- src/app/api/trail/contents/route.ts
+++ src/app/api/trail/contents/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { collection, getDocs, doc, getDoc } from "firebase/firestore";
 import { db } from "@/firebase/config";
 import { NextRequest, NextResponse } from "next/server";
